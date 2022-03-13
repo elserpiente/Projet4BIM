2796e50ea7bb0cd4efd670fc2b52717dee510bf6
elserpiente <calvindutreve42@gmail.com>
2022-03-13 17:37:07 +0100

    Clean the mess

--- AE_package.py
+++ AE_package.py
@@ -1,4 +1,88 @@
 import numpy as np
+import keras
+from keras import layers
+from keras.callbacks import TensorBoard
+from matplotlib import pyplot
+
+        
+def initAutoencoder():
+    latent_dim=512
+    #######################################################################################################################################################################
+    ###Layers###
+    #######################################################################################################################################################################
+
+    # Definition of the input -> which shape we pass in parameter
+    input_img = keras.Input(shape=(64*64*1,))
+    # Our first and only encoder layer which takes our input_img as input
+    encoded = layers.Dense(latent_dim, activation='relu')(input_img)
+    # Our first and only decoder layer which takes the output of the encoder as input
+    decoded = layers.Dense(64*64*1, activation='sigmoid')(encoded)
+
+    # We create the model of our autoencoder which takes the input images in
+    # argument and return the decoded images
+    autoencoder = keras.Model(input_img, decoded)
+
+    #######################################################################################################################################################################
+    ###Encoder###
+    #######################################################################################################################################################################
+
+    # We create the model of our encoder which takes the input images in
+    # argument and return the encoded images
+    encoder = keras.Model(input_img, encoded)
+
+    #######################################################################################################################################################################
+    ###Decoder###
+    #######################################################################################################################################################################
+
+    # We define the input of our decoder, a vector of the size of our latent space
+    encoded_input = keras.Input(shape=(latent_dim,))
+    # We get the last layer of our autoencoder so the only layer of our decoder
+    decoder_layer = autoencoder.layers[-1]
+    # We create the model of our decoder which takes the vector of our latent space in
+    # argument and return this vector decoded by our decoder layer
+    decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+
+
+    # By now we have 3 main things, our autoencoder, our encoder and our decoder
+    # they aren't in the same variables but use the same layers so when we train wether one
+    # or the other we train each layer for each things
+    return autoencoder,encoder,decoder
+
+def trainAE(autoencoder,data,epochs):
+    #######################################################################################################################################################################
+    ###Training###
+    #######################################################################################################################################################################
+
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    # Here we choose a number of 500 epochs and our data to train our autoencoder
+    # doing more epochs doesn't make a lot of sense because the loss doesn't get smaller
+    # it's mostly because the training images are really basic
+    autoencoder.fit(data, data,epochs=epochs,batch_size=256, shuffle=True,validation_data=(data, data),callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+
+    
+def testShow(dataIn,dataOut):
+    # Display of images (not interesting we won't use it in the final product)
+    # just to see all the images
+
+    n=len(dataIn)  # How many digits we will display
+    pyplot.figure(figsize=(20, 4))
+    for i in range(n):
+        # Display original
+        ax = pyplot.subplot(2, n, i + 1)
+        pyplot.imshow(dataIn[i])
+        pyplot.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # Display reconstruction
+        ax = pyplot.subplot(2, n, i + 1 + n)
+        pyplot.imshow(dataOut[i])
+        pyplot.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    pyplot.show()
+
 
 def save_in(f_name,vectors):
     fichsauv = open(f_name,"w")
