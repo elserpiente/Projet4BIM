c18152dec1b3fad998a2a11d81564e6df9ba9b79
orianebro <orianebraud1@gmail.com>
2022-03-31 16:42:41 +0200

    GA unit tests

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -8,14 +8,13 @@ import random as rd
 ### Only for tests ###
 #########################################################################################################################################
 
+
 def genome_generation (length):         # generates a np.array-genome randomly full of number between 0 and 1
     vec = []
     while len(vec) < length:
-        vec.append(rd.random())
+        vec.append(rd.randint(1,10))
     return np.array(vec)
 
-# myGenome = genome_generation(100)
-# print(myGenome)
 
 
 
@@ -25,8 +24,7 @@ def initialisation (N, T):               # creates a population of N genomes of 
         pop.append(genome_generation(T))
     return np.array(pop)
 
-# myPop = initialisation(3)
-# print(myPop)
+
 
 ###########################################################################################################################################
 
@@ -34,48 +32,52 @@ def initialisation (N, T):               # creates a population of N genomes of 
 
 # Generating a gaussian noise around each point (= vector) with a variance var
 
-def gaussian_noise (P, T, var = 0.05):
+def gaussian_noise (P, var = 0.05):
     """
-    Takes in argument a numpy array of vectors, the length of the vectors, and the variance of the gaussian noise.
+    Takes in argument a numpy array of vectors P and the variance var of the gaussian noise.
     Returns a numpy array of positive values, of the same shape as the vector in argument.
     Ex:
         input :
                 array[[0.1, 0.5, 0.4, 0.3],
                       [0.8, 0.2, 0.2, 0.9]]
-                T = 4
                 var = 0.5
+
         output :
                 array[[0.14, 0.47, 0.46, 0.23],
                       [0.85, 0.19, 0.22, 0.88]]
     """
-    cov = var*np.identity(T)
+    cov = var*np.identity(P.shape[1])
     vec_with_noise = []
     for i in range (0, P.shape[0]):
         vec_with_noise.append(np.random.multivariate_normal(P[i], cov))
     array_with_noise = np.array(vec_with_noise) 
-    positive_array_with_noise = np.abs(vec_with_noise)
-    return positive_array_with_noise           # make sure that values are > 0 !!!
+    positive_array_with_noise = np.abs(vec_with_noise)   # makes sure that values are > 0 
+    return positive_array_with_noise           
+
+
 
 
 # Crossing-over function
 # only if P has more than 1 vector
 
-def crossing_over (P, T, n, m, Tc=1):   # n has to be even
+def crossing_over (P, m, Tc=1):   
     """
-    Takes in argument a numpy array of vectors, the length of the vectors and the number of vectors (that has to be even), and the recombination rate, that we set to 1 to get only new faces.
-    Returns a numpy array of the same shape as the vector in argument.
+    Takes in argument a numpy array P of at least 2 vectors, the number of resulting vectors m, and the recombination rate Tc, that we set to 1 to get only new faces.
+    Returns a numpy array of the same shape as P in argument.
     Ex:
         input :
                 array[[0.1, 0.5, 0.4, 0.3],
                       [0.8, 0.2, 0.2, 0.9]]
-                T = 4
-                n = 2
+                
         output :
                 array[[0.1, 0.5, 0.2, 0.9],
                       [0.8, 0.2, 0.4, 0.3]]
     """
-    new_P = np.zeros((m,T))
-    
+    if P.shape[0] <= 1:
+    	raise ValueError("The input array P has to contain at least 2 vectors.")
+
+    new_P = np.zeros((m, P.shape[1]))
+
     i = 0
     while i < len(new_P):
         if rd.random() < Tc:                                                    # only works with a certain probability Tc --> here we want as many crossings as there are genomes
@@ -93,68 +95,31 @@ def crossing_over (P, T, n, m, Tc=1):   # n has to be even
             tmp2[posc:P.shape[1]] = tmp1_tail                                   # we replace the end of indc by the end of i
 
             new_P[i] = tmp1
-            if i+1<new_P.shape[0]:
+            if i+1 < new_P.shape[0]:
                 new_P[i+1] = tmp2
 
             i += 2
           
     return new_P
 
-# print(crossing_over(P=myPop, T=10))
 
 
 
+### Tests ###
 
-def getLVect(file_name):
-    """
-    Take in argument a text file with one vector saved in one line with each value separated by a space
-    Return a numpy array of shape (number of vector/line,number of value in each vector)
-    Ex:
-        file :
-                0.1 0.5 0.4/n
-                0.8 0.2 0.2/n
-        output vector :
-                [[0.1,0.5,0.4][0.8,0.2,0.2]]
-    """
-    f=open(file_name,'r')
-    nb_vect=0
-    vect=[]
-    for l in f:
-        nb_vect+=1
-        n=""
-        len_vect=0
-        for char in l:
-            if char==' ':
-                vect.append(float(n))
-                n=""
-                len_vect+=1
-            elif char!='\\' and char!='n' :
-                n+=char
-    vectors=np.ones(nb_vect*len_vect)
-    vectors=vectors*vect
-    return(np.reshape(vectors,(nb_vect,len_vect)))
+def unitary_test_1_vector ():
 
+	try:
+		myPop = initialisation(1, 10)
+		print(myPop)
+		myNoisyPop = gaussian_noise(P=myPop)
+		print(myNoisyPop)
+		print(crossing_over(P=myNoisyPop, m=6))
+		return True
+	except ValueError:
+		return False
 
-def saveLVect(file_name,vectors):
-    """
-    Take in argument a text file and a list of vectors
-    Save them in a file with one vector per line and each value separated with a space
-    Ex:
-       
-        input vector :
-                [[0.1,0.5,0.4][0.8,0.2,0.2]]
-        file :
-                0.1 0.5 0.4/n
-                0.8 0.2 0.2/n
-    """
-    f = open(file_name,"w")
-    for vector in vectors:
-        dim=len(vector)
-        str_vect=""
-        for n in vector:
-            str_vect=str_vect+str(n)+' '
-        f.write(str_vect+"\n")
-    print("Saved successfully")
+print(unitary_test_1_vector())
 
 
 """
@@ -166,11 +131,11 @@ for i in range(50):
 input_vectors=[[0.3,0.1,0.5],[0.2,0.6,0.2],[0.2,0.8,0.2]]
 input_vectors=np.array(input_vectors)
 print(input_vectors)
-n = input_vectors.shape[0]  # it works with the +1, don't ask
+n = input_vectors.shape[0]  
 t = input_vectors.shape[1]
-noisy_vectors = gaussian_noise(input_vectors, t)
+noisy_vectors = gaussian_noise(input_vectors)
 print(noisy_vectors)
-output_vectors = crossing_over(noisy_vectors, T=t, n=n)
+output_vectors = crossing_over(noisy_vectors)
 print(output_vectors)
 
 """
