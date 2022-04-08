a41372f05343c94a03e2f2ee21c051b282cef3e2
avergnaud99 <99493665+avergnaud99@users.noreply.github.com>
2022-04-08 17:37:33 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -17,6 +17,44 @@ class Window :
         interface of the application
     iteration : int
         step iterator of the application associated with the index of the pages of the application
+    screen_width : int
+        width of the screen of the computer launching the user interface
+    screen_height : int
+        height of the screen of the computer launching the user interface
+    images_data : array
+        array containing all the images of the current database 
+    database_images : array
+        array containing all the images of the current database with the right format for the checkbuttons
+    choice : array
+        array containing the indexes of the images chosen by the user
+    choice_carac : array
+        array containing the three first attributes chosen by the user
+    FrameTitle : tkinter.Frame
+        Frame of the title of the curent page
+    FrameText : tkinter.Frame
+        Frame of the text and instructions of the current page
+    FrameAttributes : tkinter.Frame
+        Frame of the attribute's selection of the first page
+    FrameFace : tkinter.Frame
+        Frame of the face's selection of the second page
+    FrameVal : tkinter.Frame
+        Frame containing all the buttons related to the validation of the current step
+    checkbuttons_attrib : array
+        array containing the variables of the checkbuttons related to the attributes of the first page
+    checkbuttons : array
+        array containing the variables of the checkbuttons related to the faces of the second page
+    attributes : array
+        array containing the name of the attributes of the pre-selection of faces
+    btn_attrib : tkinter.Button
+        button to validate the first page and step of the software
+    btn : tkinter.Button
+        button to validate the steps of the second page and run the algorithm to have new faces
+    btn_end : tkinter.Button
+        button to go the end and last page
+    var_scale : tkinter.IntVar
+        variable of type integer related to the resemblance sclae bar
+    face : tkinter.ImageTk.PhotoImage
+        image of the face which represents the final choice of the user
 
     Methods
     -------
@@ -92,7 +130,7 @@ class Window :
         self.screen_width = self.app.winfo_screenwidth()
         self.screen_height = self.app.winfo_screenheight()
 
-        self.images_data=[]
+        self.images_data = []
         self.images_data[:] = []
         
         self.choice = []
@@ -219,8 +257,8 @@ class Window :
         self.btn.place(anchor = CENTER, relx=.5, rely=.5)
 
         self.var_scale = IntVar()
-        self.scale = Scale(self.FrameVal, label = "Resemblance Scale", relief = GROOVE, borderwidth = 2, font = ('Times New Roman', int(0.012*self.screen_width), 'bold'), orient = HORIZONTAL, to = 10, length = 200, variable = self.var_scale)
-        self.scale.place(anchor = CENTER, relx = .25, rely = .5)
+        scale = Scale(self.FrameVal, label = "Resemblance Scale", relief = GROOVE, borderwidth = 2, font = ('Times New Roman', int(0.012*self.screen_width), 'bold'), orient = HORIZONTAL, to = 10, length = 200, variable = self.var_scale)
+        scale.place(anchor = CENTER, relx = .25, rely = .5)
 
         self.btn_end = Button(self.FrameVal, text = "Finish", font = ('Times New Roman', int(0.020*self.screen_width), 'bold'), state = DISABLED, command = self.end, relief = GROOVE, width = 10, height = 2)
         self.btn_end.place(anchor = CENTER, relx = .75, rely = .5)
@@ -250,7 +288,6 @@ class Window :
         self.FrameFace = Frame(self.app, relief = GROOVE, borderwidth = 5, padx = int(0.27*self.screen_width), pady = 10, background = 'beige', height = int(0.4*self.screen_height))
         self.FrameFace.pack(fill = BOTH, expand = TRUE)
         path = './database'+str(self.iteration)+'/face'+str(self.choice[0])+'.png'
-        print(self.iteration, self.choice[0])
         face = Image.open(path)
         face = face.resize((int(0.2*self.screen_width), int(0.3*self.screen_height)), Image.ANTIALIAS)
         self.face = ImageTk.PhotoImage(face)
@@ -260,10 +297,8 @@ class Window :
 
         self.FrameVal = Frame(app, padx = 10, pady = 10)
         self.FrameVal.pack(fill = BOTH, expand = TRUE)
-        self.btn_final = Button(self.FrameVal, text = "Reset", font = ('Times New Roman', int(0.020*self.screen_width), 'bold'), command = self.reset, relief = GROOVE, width = 10, height = 2)
-        self.btn_final.pack()
-
-        cleanDir()
+        btn_reset = Button(self.FrameVal, text = "Reset", font = ('Times New Roman', int(0.020*self.screen_width), 'bold'), command = self.reset, relief = GROOVE, width = 10, height = 2)
+        btn_reset.pack()
 
     def carac_choice(self) :
         """ This function is saving and sending the attributes's choice of the victim to the 'app.py' file
@@ -374,6 +409,7 @@ class Window :
         self.FrameFace.pack_forget()
         self.FrameText.pack_forget()
         self.FrameVal.pack_forget()
+        cleanDir()
         self.__init__(self.app, 0)
 
     def face_choice(self) :
@@ -557,13 +593,24 @@ class Window :
                 for f in im_choices:
                     f.save("./choice"+str(iteration+1)+"/face"+str(choices[i]+1)+".png")
                     i+=1
-def cleanDir():
-    for (dirpath, dirnames, filenames) in os.walk("../Projet4BIM"):
-            if dirpath=="../Projet4BIM":
-                for d in dirnames:
-                    if d[0:8]=="database" or d[0:6]=="choice":
-                        shutil.rmtree(d)
+def cleanDir() :
+    """ This function is useful to reset the software
     
+    It erases the databases folders and their content in order to reset the software and run it again.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    for (dirpath, dirnames, filenames) in os.walk("../Projet4BIM") :
+            if dirpath == "../Projet4BIM" :
+                for d in dirnames :
+                    if d[0:8] == "database" or d[0:6] == "choice" :
+                        shutil.rmtree(d)
 
 if __name__ == '__main__' :
     doctest.testmod(verbose = True)
